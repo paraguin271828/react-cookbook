745df4d404d3760cbc03a71ac57ecead9d4cadc6
Dennis <dennisweller@gmx.net>
2020-04-19 17:43:27 +0200

    Correct category function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
-import ReactDOM from "react-dom";
-import {BrowserRouter as Router, Switch, Route, Link, useParams} from "react-router-dom";
+import {BrowserRouter as Router, Switch, Route, useParams} from "react-router-dom";
 import "./App.css";
 import RecipeComponent from "./components/RecipeComponent";
 import Menu from "./components/Menu";
@@ -8,8 +7,8 @@ import Menu from "./components/Menu";
 // const app_url = "https://www.themealdb.com/api/json/v1/1/search.php?f=a";
 
 // const api_key = "c2230a382f204d5baf6c80cdc0569aea"; // felix
-// const api_key = "4852133db1384781b04fd81badd09bfa" // alfredo
-const api_key = "164c4f1bc5fa47919f2d66ee409af504"; // dennis
+const api_key = "4852133db1384781b04fd81badd09bfa" // alfredo
+// const api_key = "164c4f1bc5fa47919f2d66ee409af504"; // dennis
 
 const api_url = {
     random: "https://api.spoonacular.com/recipes/random/?apiKey=" + api_key + "&number=3",
@@ -18,22 +17,19 @@ const api_url = {
 }
 
 export default function App() {
-  
-
   const [recipeResult, setRecipeResult] = useState([null]);
   const [category, setCategory] = useState('random');
 
   useEffect(() => {
     fetchFunction();
-  }, []);
+  }, [category]);
 
-  const fetchFunction = (url = api_url[category]) => {
-    fetch(url)
-      .then(res => res.json())
-      .then(res => setRecipeResult(res))
-      .catch(() => console.log("Why do we still get this error?"));
-    console.log(recipeResult);
-  };
+  const fetchFunction = () => {
+      fetch(api_url[category])
+        .then(res => res.json())
+        .then(res => setRecipeResult(res))
+        .catch(err => console.log("Why do we still get this error?\n"));
+    };
 
   const fetchedRecipes =
     recipeResult.recipes &&
@@ -46,24 +42,19 @@ export default function App() {
           />
       );
     });
-    
+
   const FilteredRecipes = () => {
       let {categoryName} = useParams();
-      return <div>ghel</div>;
+      setCategory(categoryName);
+      return <div>{category}</div>;
   }
-  
+
   function deleteItem(id) {
     console.log(recipeResult);
     const tempArr = Array.from(recipeResult.recipes);
     const recipes = tempArr.filter((_, i) => i !== id);
     console.log(tempArr);
     setRecipeResult({ recipes });
-  }
-  
-  function RenderRecipes () {
-      setCategory('random');
-     fetchFunction(api_url[category]);
-      return <div>Home</div>;
   }
 
   return (
@@ -73,30 +64,38 @@ export default function App() {
         <h6>Better be fast</h6>
       </header>
       <div className="toolbar">
-        <button className="btn btn-info" onClick={() => fetchFunction(api_url[category])}>
+        <button className="btn btn-info" onClick={fetchFunction}>
           Fetch recipes
         </button>
       </div>
-      
+
       <Router>
         <Menu />
         <Switch>
           <Route path="/category/:categoryName">
             <FilteredRecipes />
+
+            {recipeResult.recipes ? (
+                fetchedRecipes
+              ) : (
+                <div className="spinner-border" role="status">
+                  <span className="sr-only">Loading...</span>
+                </div>
+              )}
           </Route>
           <Route path="/">
-          {recipeResult.recipes ? (
-              fetchedRecipes
-            ) : (
-              <div className="spinner-border" role="status">
-                <span className="sr-only">Loading...</span>
-              </div>
-            )}
+            {recipeResult.recipes ? (
+                fetchedRecipes
+              ) : (
+                <div className="spinner-border" role="status">
+                  <span className="sr-only">Loading...</span>
+                </div>
+              )}
           </Route>
         </Switch>
       </Router>
-      
-      
+
+
     </div>
   );
 }

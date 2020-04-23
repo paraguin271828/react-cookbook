4eb2e507037851bac394238bb9dcb4554ca81947
Dennis <dennisweller@gmx.net>
2020-04-23 22:39:41 +0200

    Add React Markdown module for ingredients

--- src/components/RecipeComponent.js
+++ src/components/RecipeComponent.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Markdown from "react-markdown";
 import "../styles/RecipeComponent.css";
 
 export default function RecipeComponent({ data, deleteItem }) {
   console.log("Data: " + data);
 
+  const placeholderImg = "";
   const ingredients = data.ingredients;
   //console.log(ingredients);
 
@@ -17,13 +19,13 @@ export default function RecipeComponent({ data, deleteItem }) {
     );
   });*/
 
-  return (
+  /*return (
     <div className="recipe_component">
       <div className="row">
         <div className="col">
           <div className="card">
             <img className="card-img-top" src={data.image} alt="theregoesanalttag" />
-            
+
             <div className="card-body">
               <h2 className="card-title">{data.title}</h2>
               <div className="ingredients table-responsive col-sm-12 col-md-6">
@@ -38,10 +40,37 @@ export default function RecipeComponent({ data, deleteItem }) {
                   <tbody>{data.ingredients}</tbody>
                 </table>
               </div>
-              <div className="total-time">{data.readyInMinutes}</div> 
+              <div className="total-time">{data.readyInMinutes}</div>
               <a href={data.sourceUrl} className="btn btn-primary" target="_blank" rel="noopener noreferrer">Go to recipe</a>
             </div>
-          
+
+            <div className="card-footer toolbar">
+              <button type="button" className="btn btn-danger" onClick={deleteItem}>
+                <i className="fas fa-trash-alt" /> Delete
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );*/
+
+  return (
+    <div className="recipe_component">
+      <div className="row">
+        <div className="col">
+          <div className="card">
+            <img className="card-img-top" src={data.image} alt="theregoesanalttag" />
+
+            <div className="card-body">
+              <h2 className="card-title">{data.title}</h2>
+              <div className="ingredients col-sm-12 col-md-6">
+                <Markdown source={data.ingredients} />
+              </div>
+              <div className="total-time">{data.readyInMinutes}</div>
+              <a href={data.sourceUrl} className="btn btn-primary" target="_blank" rel="noopener noreferrer">Go to recipe</a>
+            </div>
+
             <div className="card-footer toolbar">
               <button type="button" className="btn btn-danger" onClick={deleteItem}>
                 <i className="fas fa-trash-alt" /> Delete

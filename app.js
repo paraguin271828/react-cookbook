d30b1ad09f3477c6e7429a02899afe6a38da049f
Dennis <dennisweller@gmx.net>
2020-05-03 18:02:32 +0200

    Change express routing for Heroku

--- app.js
+++ app.js
@@ -18,8 +18,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use(express.static(path.join(__dirname, 'client', 'build')));
-
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
@@ -39,8 +37,12 @@ app.use(function (err, req, res, next) {
   res.render('error');
 });
 
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-});
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));// Handle React routing, return all requests to React app
+  app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
 
 module.exports = app;

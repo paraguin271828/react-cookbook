a321de900f78639daa4846affa33c6927ffe8a8d
Dennis <dennisweller@gmx.net>
2020-05-04 07:49:43 +0200

    Change wildcard route

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.use(function (err, req, res, next) {
 });
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+  res.sendFile(path.join(__dirname, 'client/build/index.html'));
 });
 
 module.exports = app;
